ceea18d130ae64a7256244002ed043848e794eff
Alberto Lacerda <alberto.p.lacerda@hotmail.com>
2021-02-28 20:15:59 -0300

    Criado migrations

---
+++ Aplicacao_01/Migrations/20210228231531_v1.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Aplicacao_01.Migrations
+{
+    public partial class v1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Formacao",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Formacao", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Curso",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Descricao = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    Duracao = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false),
+                    TipoFormacao = table.Column<int>(type: "int", nullable: false),
+                    FormacaoId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Curso", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Curso_Formacao_FormacaoId",
+                        column: x => x.FormacaoId,
+                        principalTable: "Formacao",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Curso_FormacaoId",
+                table: "Curso",
+                column: "FormacaoId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Curso");
+
+            migrationBuilder.DropTable(
+                name: "Formacao");
+        }
+    }
+}
