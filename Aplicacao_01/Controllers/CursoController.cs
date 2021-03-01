3d25cd0eb9c1a7e77d44d8dae24e99d715b9a11b
Alberto Lacerda <alberto.p.lacerda@hotmail.com>
2021-02-28 21:11:00 -0300

    Funcionando o CRUD Dos Models

---
+++ Aplicacao_01/Controllers/CursoController.cs
@@ -0,0 +1,160 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Aplicacao_01.Models;
+using Aplicacao_01.context;
+
+namespace Aplicacao_01.Controllers
+{
+    public class CursoController : Controller
+    {
+        private readonly AplicacaoData _context;
+
+        public CursoController(AplicacaoData context)
+        {
+            _context = context;
+        }
+
+        // GET: Curso
+        public async Task<IActionResult> Index()
+        {
+            var aplicacaoData = _context.Curso.Include(c => c.Formacao);
+            return View(await aplicacaoData.ToListAsync());
+        }
+
+        // GET: Curso/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var curso = await _context.Curso
+                .Include(c => c.Formacao)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (curso == null)
+            {
+                return NotFound();
+            }
+
+            return View(curso);
+        }
+
+        // GET: Curso/Create
+        public IActionResult Create()
+        {
+            ViewData["FormacaoId"] = new SelectList(_context.Formacao, "Id", "Nome");
+            return View();
+        }
+
+        // POST: Curso/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Nome,Descricao,Duracao,FormacaoId")] Curso curso)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(curso);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["FormacaoId"] = new SelectList(_context.Formacao, "Id", "Nome", curso.FormacaoId);
+            return View(curso);
+        }
+
+        // GET: Curso/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var curso = await _context.Curso.FindAsync(id);
+            if (curso == null)
+            {
+                return NotFound();
+            }
+            ViewData["FormacaoId"] = new SelectList(_context.Formacao, "Id", "Nome", curso.FormacaoId);
+            return View(curso);
+        }
+
+        // POST: Curso/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Nome,Descricao,Duracao,FormacaoId")] Curso curso)
+        {
+            if (id != curso.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(curso);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CursoExists(curso.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["FormacaoId"] = new SelectList(_context.Formacao, "Id", "Nome", curso.FormacaoId);
+            return View(curso);
+        }
+
+        // GET: Curso/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var curso = await _context.Curso
+                .Include(c => c.Formacao)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (curso == null)
+            {
+                return NotFound();
+            }
+
+            return View(curso);
+        }
+
+        // POST: Curso/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var curso = await _context.Curso.FindAsync(id);
+            _context.Curso.Remove(curso);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CursoExists(int id)
+        {
+            return _context.Curso.Any(e => e.Id == id);
+        }
+    }
+}
